52a3e7015ce01451e070a3fb26df5b3906327134
Seu Nome Completo <andregrmoura@gmail.com>
2026-08-19 21:45:11 -0400

    updat

--- js/footer-loader.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-
-  fetch("/components/footer.html")
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById("footer-container").innerHTML = data;
-    });
-
-});
